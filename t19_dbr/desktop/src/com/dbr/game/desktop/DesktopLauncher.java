7439c7c6ded008817030aa4539ff9f67a8fc5ed9
Lamrsw <32512428+Lamrsw@users.noreply.github.com>
2020-10-22 15:20:52 +0100

    Core files

---
+++ t19_dbr/desktop/src/com/dbr/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dbr.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dbr.game.DragonBoat;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DragonBoat(), config);
+	}
+}
